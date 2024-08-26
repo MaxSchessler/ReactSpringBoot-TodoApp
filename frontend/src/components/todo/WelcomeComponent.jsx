2362d107ec47af94a0af75bb12da75c1ccd3ab61
Max Schessler <schessler.max@gmail.com>
2024-08-25 19:43:36 -0500

    took out unused imports

--- frontend/src/components/todo/WelcomeComponent.jsx
+++ frontend/src/components/todo/WelcomeComponent.jsx
@@ -1,6 +1,4 @@
-import React, {useState} from "react";
-import {useNavigate} from "react-router-dom";
-import HelloWorldAPIProxy from "./API/HelloWorldAPIProxy";
+import React from "react";
 import {useAuth} from "./security/AuthorizationContext";
 
 const WelcomeComponent = () => {
